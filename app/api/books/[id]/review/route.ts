521cfb18ec29eec77ab06a1a27762686b23db9f6
Hisham Abualhaj <hishamecommerce@gmail.com>
2025-10-05 12:14:27 +0300

    route handler async for review

--- app/api/books/[id]/review/route.ts
+++ app/api/books/[id]/review/route.ts
@@ -1,8 +1,8 @@
 import fetchData from "@/app/utils/fetchData";
 import { NextResponse } from "next/server";
 
-export function GET(request: Request, { params }: { params: { id: string } }) {
-  const { id } = params;
+export async function GET(request: Request, context: { params: Promise<{ id: string }> }) {
+  const { id } = await context.params;
 
   const data: Record<string, any>[] = fetchData(
     "../data/reviews.json",
